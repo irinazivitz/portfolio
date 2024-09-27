b8e1f36c6c69ec9d4b6a6d52a7e770ecb9ee0bec
irinazivitz <irina.woodward@gmail.com>
2024-09-27 11:03:59 -0400

    Updated stules and layout

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,39 +2,48 @@
   <div class="home">
     <h1>Hi, my name is Irina Zivitz</h1>
   </div>
-  <h5 class="text-wrap ms-20 mt-3">
-    I'm a  frontend developer.  Check out my recent work and get in touch to chat about potential projects. 
-  </h5>
-  <hr>
-  <a href="https://charming-crisp-9672b3.netlify.app">
-      <h4> MyTide app</h4>
-  </a>
+  <div class="content-container">
+
   
-  <div class="d-flex align-items-center">
+      <h5 class=" mt-3">
+        I'm a  frontend developer.  Check out my recent work and get in touch to chat about potential projects. 
+      </h5>
+      <hr>
+      
       <a href="https://charming-crisp-9672b3.netlify.app">
-        <img class="rounded float-image ms-3 mt-3 mb-3" src="../assets/tideAppImage.jpg" alt="tideApp image"> 
+          <h4> MyTide app</h4>
       </a>
-     
-      <div class="ms-5 me-3">
-        <p>
-          I live near the Atlantic Ocean and often need to know the tide, 
-          so I know where to put my beach chair or should I promise my 5yo tidal 
-          pools or not. Of course, most people use tide info for much serious purpose, 
-          like surfing or fishing or actually navigating boats. I designed this 
-          single-purpose app to be informative and minimalistic, to leave more 
-          mindspace for fun and actual waves.
-        </p>  
-        <div class="custom-button"> Vue.js</div>
-        <div class="custom-button"> JavaScript</div>
-        <div class="custom-button"> CSS</div>
-        <div class="custom-button"> HTML</div>
-        <div class="mt-3">
-            <a href="https://charming-crisp-9672b3.netlify.app"> deployed site</a> | 
-            <a href="https://github.com/irinazivitz/tide-app"> GitHub</a> 
-  
-        </div>
-      </div>   
-  </div>
+      
+      <div class="container">
+        <div class="row align-items-center">
+          <div class="col-md-6 col-sm-12">
+              <a href="https://charming-crisp-9672b3.netlify.app">
+                  <img class="img-fluid rounded float-image ms-3 mt-3 mb-3" 
+                  src="../assets/tideAppImage.jpg" alt="tideApp image"> 
+              </a>
+          </div>
+
+          <div class="col-md-6 col-sm-12">
+            <p>
+              I live near the Atlantic Ocean and often need to know the tide, 
+              so I know where to put my beach chair or should I promise my 5yo tidal 
+              pools or not. Of course, most people use tide info for much serious purpose, 
+              like surfing or fishing or actually navigating boats. I designed this 
+              single-purpose app to be informative and minimalistic, to leave more 
+              mindspace for fun and actual waves.
+            </p>  
+            <div class="custom-button"> Vue.js</div>
+            <div class="custom-button"> JavaScript</div>
+            <div class="custom-button"> CSS</div>
+            <div class="custom-button"> HTML</div>
+            <div class="mt-3">
+                <a href="https://charming-crisp-9672b3.netlify.app"> deployed site</a> | 
+                <a href="https://github.com/irinazivitz/tide-app"> GitHub</a> 
+            </div>
+          </div>   
+        </div>  
+      </div>
+    </div>
   
 </template>
 
@@ -44,7 +53,6 @@
 
 <style scoped>
 .float-image{
-  width:400px;
   box-shadow: 0px 8px 15px rgba(0, 0, 0, 0.3);
   transition: transform 0.3s ease, box-shadow 0.3s ease;
 }
@@ -64,6 +72,26 @@
 a {
   color:rgb(55, 112, 70);
   text-decoration: none;
+}
+
+.col-md-6 {
+  padding: 20px;
+}
+.container {
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+}
+h5 {
+  text-align: center;
+}
+.content-container {
+  max-width: 1100px;
+  margin: 0 auto;
+}
+hr {
+  width: 100%;
+  margin: 20px 0;
 }
 
 
